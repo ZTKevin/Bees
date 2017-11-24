ded0bb9bec4f1458faf9ef1579684474278c9d41
Lion <hongcaiyu@live.com>
2017-11-24 11:02:35 +0800

    创建项目

---
+++ Bees/Bees.h
@@ -0,0 +1,19 @@
+//
+//  Bees.h
+//  Bees
+//
+//  Created by hongcaiyu on 24/11/17.
+//  Copyright © 2017年 Lion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Bees.
+FOUNDATION_EXPORT double BeesVersionNumber;
+
+//! Project version string for Bees.
+FOUNDATION_EXPORT const unsigned char BeesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bees/PublicHeader.h>
+
+
