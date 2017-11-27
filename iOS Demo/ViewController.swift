ae75d8cde35d09341d6460b1fafde72852df3db8
Lion <hongcaiyu@live.com>
2017-11-27 09:35:34 +0800

    添加许可

--- iOS Demo/ViewController.swift
+++ iOS Demo/ViewController.swift
@@ -3,8 +3,26 @@
 //  iOS Demo
 //
 //  Created by hongcaiyu on 24/11/17.
-//  Copyright © 2017年 Lion. All rights reserved.
 //
+//  Copyright (c) 2017 Caiyu Hong <hongcaiyu@live.com>
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
 
 import UIKit
 import Bees
@@ -48,7 +66,7 @@ class ViewController: UIViewController {
         rightBottomView.bee.left == centerView.bee.right + 10
         rightBottomView.bee.top == centerView.bee.top
         
-        self.superView.layoutIfNeeded()
+        superView.layoutIfNeeded()
         
         
         UIView.animateKeyframes(withDuration: 3, delay: 0, options: [.repeat, .calculationModeLinear, .autoreverse], animations: {
